a77cc6f1c41bd0d1b22884d0a0beca9bfe39901e
Pedro Lucena <44994733+pedrolucena05@users.noreply.github.com>
2026-03-02 01:39:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -605,7 +605,13 @@ def api_store_message():
         "message": msg.to_dict() if msg else None
     })
 
-@app.route("/bot", methods=["GET", "POST"])
+@app.post("/bot")
+def receive():
+    data = request.get_json(silent=True) or {}
+    print("EVENTO RECEBIDO:", data)
+    return "OK", 200
+
+'''@app.route("/bot", methods=["GET", "POST"])
 def webhook_handler():
     if request.method == "GET":
         mode = request.args.get("hub.mode")
@@ -688,7 +694,7 @@ def webhook_handler():
     worker["queue"].put(text)
     worker["last_active"] = time.time()
 
-    return jsonify({"status": "queued"}), 200
+    return jsonify({"status": "queued"}), 200'''
 
 
 def terminal_loop():
@@ -811,4 +817,5 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
 
         print("Servidor finalizado.")
+
 
