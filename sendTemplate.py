bcdec8a19d2fac13178ffb2465fc4272dc76509a
pedrolucena05 <pedrolucena0523@gmail.com>
2026-05-28 04:55:00 -0300

    Cron python file

---
+++ sendTemplate.py
@@ -0,0 +1,49 @@
+import os
+import requests
+from sqlalchemy import update
+from tableClasses import Cliente
+from dbConfig import db
+
+WHATSAPP_ACCESS_TOKEN = os.getenv("WHATSAPP_ACCESS_TOKEN")
+DEFAULT_PHONE_NUMBER_ID = os.getenv("WHATSAPP_PHONE_NUMBER_ID")
+GRAPH_API_VERSION = os.getenv("GRAPH_API_VERSION", "v24.0")
+VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
+
+session = db.session
+
+phones = (session.query(Cliente.phone).filter(Cliente.templateNeeded == True).all())
+
+phones = [phone for (phone,) in phones]
+
+url = f"https://graph.facebook.com/{GRAPH_API_VERSION}/{DEFAULT_PHONE_NUMBER_ID}/messages"
+
+headers = {
+    "Authorization": f"Bearer {WHATSAPP_ACCESS_TOKEN}",
+    "Content-Type": "application/json"
+}
+
+for phone in phones:
+    payload = {
+        "messaging_product": "whatsapp",
+        "to": phone,
+        "type": "template",
+        "template": {
+            "name": "reativar_a_conversa",
+            "language": {
+                "code": "pt_BR"
+            }
+        }
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
+
+    if response.status_code in [200, 201]:
+        stmt = (
+            update(Cliente)
+            .where(Cliente.phone == phone)
+            .values(templateNeeded=False)
+        )
+
+        session.execute(stmt)
+
+session.commit()
